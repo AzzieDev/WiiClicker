27f522882351b6bfcb195d9c5fa939db8b3c249f
David Katz <dkatz10@students.towson.edu>
2022-05-05 01:40:07 -0400

    clean more

--- main.c
+++ main.c
@@ -30,47 +30,12 @@ static struct xwii_iface *iface;
 static unsigned int mode = MODE_ERROR;
 static bool freeze = false;
 
-/* error messages */
-
-static void nextSlide() {
-    hThread = GetWindowThreadProcessId(hwnd,&dwPID);
-    if (dwPID == dwProcessID && hThread!= NULL ) {
-        PostThreadMessage( hThread, WM_KEYDOWN,'A',1);
-    }
-}
-
-static void print_info(const char *format, ...) {
-    va_list list;
-    char str[58 + 1];
-
-    va_start(list, format);
-    vsnprintf(str, sizeof(str), format, list);
-    str[sizeof(str) - 1] = 0;
-    va_end(list);
-
-    mvprintw(22, 22, "                                                          ");
-    mvprintw(22, 22, "%s", str);
-    refresh();
-}
-
-static void print_error(const char *format, ...) {
-    va_list list;
-    char str[58 + 80 + 1];
-
-    va_start(list, format);
-    vsnprintf(str, sizeof(str), format, list);
-    if (mode == MODE_EXTENDED)
-        str[sizeof(str) - 1] = 0;
-    else
-        str[58] = 0;
-    va_end(list);
-
-    mvprintw(23, 22, "                                                          ");
-    if (mode == MODE_EXTENDED)
-        mvprintw(23, 80, "                                                                                ");
-    mvprintw(23, 22, "%s", str);
-    refresh();
-}
+// static void nextSlide() {
+//     hThread = GetWindowThreadProcessId(hwnd,&dwPID);
+//     if (dwPID == dwProcessID && hThread!= NULL ) {
+//         PostThreadMessage( hThread, WM_KEYDOWN,'A',1);
+//     }
+// }
 
 /* key events */
 
@@ -137,22 +102,6 @@ static void key_clear(void) {
     }
 }
 
-static void key_toggle(void) {
-    int ret;
-
-    if (xwii_iface_opened(iface) & XWII_IFACE_CORE) {
-        xwii_iface_close(iface, XWII_IFACE_CORE);
-        key_clear();
-        print_info("Info: Disable key events");
-    } else {
-        ret = xwii_iface_open(iface, XWII_IFACE_CORE |
-                                     XWII_IFACE_WRITABLE);
-        if (ret)
-            print_error("Error: Cannot enable key events: %d", ret);
-        else
-            print_info("Info: Enable key events");
-    }
-}
 
 
 /* rumble events */
@@ -168,7 +117,7 @@ static void rumble_toggle(void) {
     on = !on;
     ret = xwii_iface_rumble(iface, on);
     if (ret) {
-        print_error("Error: Cannot toggle rumble motor: %d", ret);
+       // print_error("Error: Cannot toggle rumble motor: %d", ret);
         on = !on;
     }
 
@@ -183,27 +132,7 @@ static void led_show(int n, bool on) {
     mvprintw(5, 59 + n * 5, on ? "(#%i)" : " -%i ", n + 1);
 }
 
-static void led_toggle(int n) {
-    int ret;
 
-    led_state[n] = !led_state[n];
-    ret = xwii_iface_set_led(iface, XWII_LED(n + 1), led_state[n]);
-    if (ret) {
-        print_error("Error: Cannot toggle LED %i: %d", n + 1, ret);
-        led_state[n] = !led_state[n];
-    }
-    led_show(n, led_state[n]);
-}
-
-static void led_refresh(int n) {
-    int ret;
-
-    ret = xwii_iface_get_led(iface, XWII_LED(n + 1), &led_state[n]);
-    if (ret)
-        print_error("Error: Cannot read LED state");
-    else
-        led_show(n, led_state[n]);
-}
 
 /* battery status */
 
@@ -222,9 +151,9 @@ static void battery_refresh(void) {
     uint8_t capacity;
 
     ret = xwii_iface_get_battery(iface, &capacity);
-    if (ret)
-        print_error("Error: Cannot read battery capacity");
-    else
+    if (!ret)
+        //print_error("Error: Cannot read battery capacity");
+    //else
         battery_show(capacity);
 }
 
@@ -235,46 +164,21 @@ static void devtype_refresh(void) {
     char *name;
 
     ret = xwii_iface_get_devtype(iface, &name);
-    if (ret) {
-        print_error("Error: Cannot read device type");
-    } else {
+    if (!ret) {
+       // print_error("Error: Cannot read device type");
+    //} else {
         mvprintw(9, 28, "                                                   ");
         mvprintw(9, 28, "%s", name);
         free(name);
     }
 }
 
-/* extension type */
-
-static void extension_refresh(void) {
-    int ret;
-    char *name;
-
-    ret = xwii_iface_get_extension(iface, &name);
-    if (ret) {
-        print_error("Error: Cannot read extension type");
-    } else {
-        mvprintw(7, 54, "                      ");
-        mvprintw(7, 54, "%s", name);
-        free(name);
-    }
-
-    if (xwii_iface_available(iface) & XWII_IFACE_MOTION_PLUS)
-        mvprintw(7, 77, "M+");
-    else
-        mvprintw(7, 77, "  ");
-}
 
 /* basic window setup */
 
 static void refresh_all(void) {
     battery_refresh();
-    led_refresh(0);
-    led_refresh(1);
-    led_refresh(2);
-    led_refresh(3);
     devtype_refresh();
-    extension_refresh();
 
     if (geteuid() != 0)
         mvprintw(20, 22, "Warning: Please run as root! (sysfs+evdev access needed)");
@@ -332,22 +236,19 @@ static void handle_watch(void) {
     static unsigned int num;
     int ret;
 
-    print_info("Info: Watch Event #%u", ++num);
+    //print_info("Info: Watch Event #%u", ++num);
 
     ret = xwii_iface_open(iface, xwii_iface_available(iface) |
                                  XWII_IFACE_WRITABLE);
-    if (ret)
-        print_error("Error: Cannot open interface: %d", ret);
+    //if (ret)
+        //print_error("Error: Cannot open interface: %d", ret);
 
     refresh_all();
 }
 
 /* keyboard handling */
 
-static void freeze_toggle(void) {
-    freeze = !freeze;
-    print_info("Info: %sreeze screen", freeze ? "F" : "Unf");
-}
+
 
 static int keyboard(void) {
     int key;
@@ -360,26 +261,8 @@ static int keyboard(void) {
         case KEY_RESIZE:
             handle_resize();
             break;
-        case 'k':
-            key_toggle();
-            break;
         case 'r':
             rumble_toggle();
-            break;
-        case '1':
-            led_toggle(0);
-            break;
-        case '2':
-            led_toggle(1);
-            break;
-        case '3':
-            led_toggle(2);
-            break;
-        case '4':
-            led_toggle(3);
-            break;
-        case 'f':
-            freeze_toggle();
             break;
         case 's':
             refresh_all();
@@ -404,26 +287,26 @@ static int run_iface(struct xwii_iface *iface) {
     fds_num = 2;
 
     ret = xwii_iface_watch(iface, true);
-    if (ret)
-        print_error("Error: Cannot initialize hotplug watch descriptor");
+    // if (ret)
+    //     print_error("Error: Cannot initialize hotplug watch descriptor");
 
     while (true) {
         ret = poll(fds, fds_num, -1);
         if (ret < 0) {
             if (errno != EINTR) {
                 ret = -errno;
-                print_error("Error: Cannot poll fds: %d", ret);
+             //   print_error("Error: Cannot poll fds: %d", ret);
                 break;
             }
         }
 
         ret = xwii_iface_dispatch(iface, &event, sizeof(event));
         if (ret) {
-            if (ret != -EAGAIN) {
-                print_error("Error: Read failed with err:%d",
-                            ret);
-                break;
-            }
+            // if (ret != -EAGAIN) {
+            //     print_error("Error: Read failed with err:%d",
+            //                 ret);
+            //     break;
+            // }
         } else if (!freeze) {
             switch (event.type) {
                 case XWII_EVENT_WATCH:
@@ -488,7 +371,7 @@ static char *get_dev() {
     xwii_monitor_unref(mon);
 
     if (!ent)
-        printf("Cannot find device with number #%d\n", 1);
+        printf("Cannot find device.");
 
     return ent;
 }
@@ -552,14 +435,14 @@ int main(int argc, char **argv) {
         ret = xwii_iface_open(iface,
                               xwii_iface_available(iface) |
                               XWII_IFACE_WRITABLE);
-        if (ret)
-            print_error("Error: Cannot open interface: %d",
-                        ret);
+        // if (ret)
+        //     print_error("Error: Cannot open interface: %d",
+        //                 ret);
 
         ret = run_iface(iface);
         xwii_iface_unref(iface);
         if (ret) {
-            print_error("Program failed; press any key to exit");
+          //  print_error("Program failed; press any key to exit");
             refresh();
             timeout(-1);
             getch();
